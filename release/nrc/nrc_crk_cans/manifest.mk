31d9f87cdefbeb1b67bcbcc78da78874a7416fb0
Eddie Antonio Santos <easantos@ualberta.ca>
2019-11-15 09:28:57 -0700

    Automate copying files to keymanapp/keyboards 🙃

---
+++ release/nrc/nrc_crk_cans/manifest.mk
@@ -0,0 +1,14 @@
+# Files that will be copied to the distribution.
+PACKAGE_FILES = HISTORY.md \
+				LICENSE.md \
+				README.md \
+				nrc_crk_cans.keyboard_info \
+				nrc_crk_cans.kpj \
+				source/help/nrc_crk_cans.php \
+				$(wildcard source/help/*.jpg) \
+				source/nrc_crk_cans.keyman-touch-layout \
+				source/nrc_crk_cans.kmn \
+				source/nrc_crk_cans.kps \
+				source/readme.htm \
+				source/welcome/welcome.htm \
+				$(wildcardsource/welcome/ *.jpg)
